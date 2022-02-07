f0241fdee7d556f3640b9d26af4d47cdd398a541
S. Espinet <s.lou.espinet@gmail.com>
2022-02-07 12:01:31 -0800

    WIP trying to display results from different endpoint
    Co-Authored-By: Hayley M <hayleymcvay8@gmail.com>
    Co-authored by: Marisa Edgar <marisaedgar1212@gmail.com>

--- src/index.js
+++ src/index.js
@@ -28,4 +28,25 @@ $(document).ready(function() {
       $('.showWindSpeed').text(`The wind speed is ${response.wind.speed} mph.`);
     }
   });
+  $("#forecast").click(function() {
+    const city = $("#location").val();
+    $("#location").val();
+
+    let request2 = new XMLHttpRequest();
+    const urlForecast = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.API_KEY}`;
+
+    request2.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getForecast(response);
+      }
+    };
+
+    request2.open("GET", urlForecast, true);
+    request2.send();
+
+    function getForecast(response) {
+      $('.showForecast').text(`The 5-day forecast in ${city} is ${response.list}`);
+    }
+  })
 });
